146b467e7c7a1e7d6917e59193aa99a51cd8a39e
dennyman <ducanto11@yandex.ru>
2024-08-01 22:00:32 +0300

    some more features of re module

--- check_regex.py
+++ check_regex.py
@@ -1,9 +1,12 @@
 import re
 
 def main():
-    phoneNumRegex  =re.compile(r'\d{3}-\d{3}-\d{4}')
+    phoneNumRegex = re.compile(r'(\d{3})-(\d{3}-\d{4})')
     match_object = phoneNumRegex.search("My phone number: 415-555-4242.")
-    print("Found phone number: " + match_object.group())
+    print("Found phone number:", match_object.group)
+    print("Region code:", match_object.group(1))
+    print("Number:", match_object.group(2))
+    print("All in all:", match_object.groups())
 
 if __name__ == "__main__":
     main()
